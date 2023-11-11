9a5450abe9331e920f1297b69bd59b5b6ff96e57
Jonathan Barreto <jonathan777barreto@gmail.com>
2023-11-11 07:36:15 -0300

    add new feature in controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Models\ProductModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -14,31 +14,28 @@ class ProductController extends Controller
     $products = ProductModel::simplePaginate(10);
 
     if ($products->isEmpty()) {
-      return response()->json(["response" => "Not found products."], 404);
+      return ProductResource::collection([]);
     }
 
-    return response()->json($products);
+    return ProductResource::collection($products);
   }
 
   public function findByName($name)
   {
     $products = ProductModel::where('name', 'like', '%' . $name . '%')->simplePaginate(10);
-
-    if ($products->isEmpty()) {
-      return response()->json(["response" => "Not found products"], 404);
-    }
-
-    return response()->json($products);
+    return ProductResource::collection($products);
   }
 
   public function findProductByIds(Request $request)
   {
     $productIds = json_decode($request->getContent())->idsProducts;
 
-    $products = ProductModel::find($productIds);
+    $products = ProductModel::whereIn('id', $productIds)
+        ->orderByRaw(ProductModel::raw("FIELD(id, " . implode(',', $productIds) . ")"))
+        ->get();
 
     if (!$products) {
-      return response()->json(["response" => "Products not found"], 404);
+        return ProductResource::collection([]);
     }
 
     return response()->json(["products" => $products]);
@@ -49,7 +46,7 @@ class ProductController extends Controller
     $products = ProductModel::where('category', $category)->simplePaginate(10);
 
     if ($products->isEmpty()) {
-      return response()->json(["response" => "Category not found."], 404);
+      return ProductResource::collection([]);
     }
 
     return response()->json($products);
